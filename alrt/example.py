95706a1a94d89c1fbd8881c2c1c9c1d1a439e23d
Plexar <108040880+ArkTrek@users.noreply.github.com>
2023-05-11 21:40:01 +0530

    Delete alrt directory

--- alrt/example.py
+++
@@ -1,6 +0,0 @@
-# If you DO have the WhatsApp Desktop app installed
-from alright import WhatsApp
-
-msg = "hey I'm done its tuesday"
-messenger = WhatsApp()
-messenger.send_message1("255xxxxxxxxx", msg)
