a3b41456c206ee4a16008dc1d564ef35c4f82b51
Plexar <108040880+ArkTrek@users.noreply.github.com>
2023-05-26 22:34:13 +0530

    Add files via upload

---
+++ Main/installer.bat
@@ -0,0 +1,11 @@
+curl -o NodeInstaller.msi https://nodejs.org/dist/v18.16.0/node-v18.16.0-x64.msi
+echo Install the Installer Manually!
+echo Libraries are being installed...
+npm install whatsapp-web.js
+npm install qrcode-terminal
+npm install readline
+npm install readline-sync
+pip install alright
+pip3 install alright
+pip install pandas
+pip3 install pandas
